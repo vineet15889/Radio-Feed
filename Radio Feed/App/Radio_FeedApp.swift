05f816dfb098665fb93b732b29145c522a78d6ee
Vineet Kumar Rai <vineetrai@outlook.com>
2026-03-28 09:27:57 +0530

    Basic setup

---
+++ Radio Feed/App/Radio_FeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  Radio_FeedApp.swift
+//  Radio Feed
+//
+//  Created by Vineet Rai on 28-Mar-26.
+//
+
+import SwiftUI
+
+@main
+struct Radio_FeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Radio Feed/Presentation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Radio Feed
+//
+//  Created by Vineet Rai on 28-Mar-26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
